bf0856e6110eaf6eec0a5ecc43f66479cfc1d8fc
front-desk-casasbrancas <front@casasbrancas.com.br>
2026-07-05 08:02:53 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cb-transfers-v9';
+const CACHE = 'cb-transfers-v10';
 const ASSETS = [
   'menu.html', 'index.html', 'dashboard.html', 'motorista.html', 'motorista-app.html', 'transfer.html', 'financeiro.html',
   'acompanhamento.html', 'avaliacao.html', 'avaliacoes.html',
